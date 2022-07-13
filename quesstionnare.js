7ea351b08953a8b6d520f9db556c285611eb84cc
Erhan <erhankubanych@gmail.com>
2022-07-13 16:22:02 +0600

    fead:added field check in form1

--- quesstionnare.js
+++ quesstionnare.js
@@ -1,14 +1,32 @@
-const $form = document.querySelector('#form1');
- const $elements = document.querySelectorAll('input , select');
-   const $user = {};
+const form = document.querySelector('#form1');
 
-
-
-$form.addEventListener("submit", () => {
-   
-   for (let i = 0; i < $elements.length; i++) {
-      $user[$elements[i].name] = $elements[i].value
+const infoUser = () => {
+   const user = {};
+   for (let i = 0; i < elements.length; i++) {
+      user[elements[i].name] = elements[i].value
    }
-   localStorage.setItem('form1', JSON.stringify($user))
-   window.location.href='income_data.html'
+   localStorage.setItem('form1', JSON.stringify(user))
+   
+}
+
+const fillingCheck = (elements) => {
+   let check = 0;
+   for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('input', () => {
+            elements[i].classList.remove("error");
+         })
+      if (elements[i].value === '') {
+         elements[i].classList.add('error');
+         check++;
+      }
+   }
+   return check === 0;
+};
+
+form.addEventListener("submit", () => {
+   const elements = document.querySelectorAll('input , select');
+   if (fillingCheck(elements)) {
+      window.location.href = 'income_data.html'
+      infoUser()
+   }
 })
