f45f7394545ff0c67181e9bc7d08663be7a87d7e
Erhan <erhankubanych@gmail.com>
2022-07-14 18:27:13 +0600

    feat: added field fill check and changed html, css

--- income_data.js
+++ income_data.js
@@ -1,13 +1,31 @@
-const $form2 = document.querySelector('#form2')
+const form2 = document.querySelector('#form2')
 
-$form2.addEventListener('submit', () => {
-   const $elements = document.querySelectorAll('input , select');
-   const $user = {}
-   
-   for (let i = 0; i < $elements.length; i++) { 
-      $user[$elements[i].name] = $elements[i].value;
-   }
+const infoUser = () => {
+	const user = {};
+	for (let i = 0; i < elements.length; i++) {
+		user[elements[i].name] = elements[i].value;
+	}
+	localStorage.setItem("form1", JSON.stringify(user));
+};
 
-   localStorage.setItem("form2", JSON.stringify($user));
-   window.location.href='request.html'
+const fillingCheck = (elements) => {
+	let check = 0;
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].addEventListener("input", () => {
+			elements[i].classList.remove("error");
+		});
+		if (elements[i].value === "") {
+			elements[i].classList.add("error");
+			check++;
+		}
+	}
+	return check === 0;
+};
+
+form2.addEventListener('submit', () => {
+   const elements = document.querySelectorAll('input , select');
+   if (fillingCheck(elements)) {
+			 window.location.href='request.html'
+			infoUser();
+		}
 })
