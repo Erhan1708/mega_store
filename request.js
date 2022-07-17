366ca6fe24aa9670e93deccaa2d6a85e05974ff3
Erhan <erhankubanych@gmail.com>
2022-07-17 14:58:32 +0600

    feat:there were minor changes in the names of the input

--- request.js
+++ request.js
@@ -1,6 +1,25 @@
 const $userObject = JSON.parse(localStorage.getItem("form1"));
 const $userObject2 = JSON.parse(localStorage.getItem("form2"));
 
+fetch($userObject, $userObject2)
+   .then((res) => res.json())
+   .then((data) => { 
+      let info = document.querySelector(".info");
+      Object.keys(data.value)
+      info.insertAdjacentHTML(
+         'afterbegin',
+         ``
+      )
+   })
+
+
+
+
+
+
+
+
+
 const result = {
 		form1: $userObject,
 		form2: $userObject2,
